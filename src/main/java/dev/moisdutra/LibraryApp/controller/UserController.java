5b89b4229a6f0b06ffeedd7a69b33edbaccfa5dd
moisdutra <moisesbrdg3@hotmail.com>
2026-05-16 12:26:37 -0300

    feat: Implements the User Controller and it's routes

--- src/main/java/dev/moisdutra/LibraryApp/controller/UserController.java
+++ src/main/java/dev/moisdutra/LibraryApp/controller/UserController.java
@@ -1,8 +1,12 @@
 package dev.moisdutra.LibraryApp.controller;
 
+import dev.moisdutra.LibraryApp.model.UserModel;
 import dev.moisdutra.LibraryApp.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -14,5 +18,50 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/listusers")
+    public ResponseEntity<List<UserModel>> listUsers() {
+        List<UserModel> users = userService.listUsers();
+        return ResponseEntity.status(HttpStatus.OK).body(users);
+    }
+
+    @GetMapping("/listuser/{id}")
+    public ResponseEntity<?> getUser(@PathVariable Long id) {
+        UserModel user = userService.listUserById(id);
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+    }
+
+    @PostMapping("/adduser")
+    public ResponseEntity<UserModel> addUser(@RequestBody UserModel newUser) {
+        UserModel user = userService.createUser(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
+    @PatchMapping("/updateuser/{id}")
+    public ResponseEntity<?> updateUser(
+            @PathVariable Long id,
+            @RequestBody UserModel updatedUser
+    ) {
+        UserModel user = userService.updateUser(id, updatedUser);
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.OK).body(user);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+    }
+
+    @DeleteMapping("/deleteuser/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        if (userService.listUserById(id) != null) {
+            userService.deleteUser(id);
+            return ResponseEntity.status(HttpStatus.OK).body("User deleted.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
+        }
+    }
 
 }
